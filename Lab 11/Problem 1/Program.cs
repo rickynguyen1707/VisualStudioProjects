791305e41211bc63e89fa3bde3f5437835a57b65
KN <das>
2019-11-26 13:37:57 -0500

    2nd commit

---
+++ Lab 11/Problem 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] obama = { "Barack", "Hussein", "Obama" };
+
+            PrintElements(obama);
+        }
+        static void PrintElements(string[] obamaname)
+        {
+            for (int i = 0; i < obamaname.Length; i++)
+            {
+                Console.WriteLine(obamaname[i]);
+            }
+        }
+    }
+}
---
+++ Lab 11/Problem 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19 };
+
+
+            Console.WriteLine($"sum is {AddElements(primes)}");
+
+
+        }
+        static int AddElements(int[] anArray)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < anArray.Length; i++)
+            {
+                sum += anArray[i];
+            }
+            return sum;
+        }
+    }
+}
